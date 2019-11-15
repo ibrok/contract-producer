2ebe35eaf523e1f60577507b4c594ab3fe0b61d2
ibrok <ingridbrok@gmail.com>
2019-11-15 12:04:45 +0100

    initial commit

---
+++ app/src/test/java/com/contract/example/producer/ContractProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contract.example.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContractProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
